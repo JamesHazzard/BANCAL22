ad9c46dd0a817fcd9588e2b8e14d4d5290c69da3
JamesHazzard <j.hazzard20@imperial.ac.uk>
2022-04-05 17:06:08 +0100

    Building main algorithm

---
+++ algorithm/GASWAM.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+def calculate_prior(x):
+    return 1
+
+def calculate_likelihood(x):
+    return 1
+
+def run_algorithm(n_trials, n_burnin, x0, priors, data_selection, data_length, data_xenolith, data_plate, data_adiabat, data_attenuation, data_viscosity):
+        alpha_ideal = 0.234
+        n_params = len(x0)
+        gamma = (2.38**2)/n_params
+        model = np.zeros((n_params, n_trials))
+        accepted_model = []
+        x = x0
+        prior_x = calculate_prior(x)
+        likelihood_x = calculate_likelihood(x)
+        proposal_covariances = np.diag(np.concatenate((priors[1], np.full(n_params-len(priors[1]), 0.1**2))))
+        S0=np.linalg.cholesky(proposal_covariances)
+        print(proposal_covariances)
+        print(S0)
+        print(prior_x)
+        print(likelihood_x)
+
+        for i in range(99):
+            model[:,i] = x
+            U=np.random.multivariate_normal(np.zeros(n_params), np.eye(n_params))
+            y = x + np.matmul(S0,U)
+            prior_y = calculate_prior(y)
+            likelihood_y = calculate_likelihood(y) 
+            #alpha = min(1, np.exp(likelihood_y - likelihood_x)*(prior_y / prior_x))
+            alpha = 1
+            u = np.random.uniform(low = 0, high = 1, size = 1)
+            if u < alpha: 
+                x = y
+                prior_x = prior_y
+                likelihood_x = likelihood_y
+                accepted_model.append(x)
+            
+        for i in range(n_trials - 99):
+            model[:,i] = x
+            U=np.random.multivariate_normal(np.zeros(n_params), np.eye(n_params))
+            if gamma + ((i)**(-0.5))*(alpha - alpha_ideal) > 0:
+                gamma += ((i)**(-0.5))*(alpha - alpha_ideal)
+            C = (gamma)*np.cov(accepted_model, rowvar = False) + (1e-30*np.eye(n_params))
+            S = np.linalg.cholesky(C)
+            y = x + np.matmul(S,U)
+            prior_y = calculate_prior(y)
+            likelihood_y = calculate_likelihood(y) 
+            # alpha = min(1, np.exp(likelihood_y - likelihood_x)*(prior_y / prior_x))
+            alpha = 1
+            u = np.random.uniform(low = 0, high = 1, size = 1)
+            if u < alpha: 
+                x = y
+                prior_x = prior_y
+                likelihood_x = likelihood_y
+                accepted_model.append(x)
+
+
+
+
--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 import numpy as np
 import shutil
+from GASWAM import * 
 
 data_type = ['xenolith', 'plate', 'adiabat', 'attenuation', 'viscosity']
-data_name = ['nodule_obs_all.zTVslln', '', '', '', '']
-data_xenolith = np.zeros(1)
-data_plate = np.zeros(1)
-data_adiabat = np.zeros(1)
-data_attenuation = np.zeros(1)
-data_viscosity = np.zeros(1)
+data_name = ['nodule_obs_all.zTVslln', 'plate.VseTz', 'adiabat.VseTz', 'attenuation.QeVsz', 'viscosity.neVsz']
+data_xenolith = []
+data_plate = []
+data_adiabat = []
+data_attenuation = []
+data_viscosity = []
 data_selection = np.loadtxt('./data/data_selection.txt', skiprows = 1)
 if data_selection[0] == 1:
     data_xenolith = np.loadtxt('./data/'+data_type[0]+'/'+data_name[0])
     data_xenolith = np.split(data_xenolith, np.where(np.diff(data_xenolith[:,5]))[0]+1)
+if data_selection[1] == 1:
+    data_plate = np.loadtxt('./data/'+data_type[1]+'/'+data_name[1], skiprows = 1).T
+if data_selection[2] == 1:
+    data_adiabat = np.loadtxt('./data/'+data_type[2]+'/'+data_name[2], skiprows = 1).T
+if data_selection[3] == 1:
+    data_attenuation = np.loadtxt('./data/'+data_type[3]+'/'+data_name[3], skiprows = 1).T
+if data_selection[4] == 1:
+    data_viscosity = np.loadtxt('./data/'+data_type[4]+'/'+data_name[4], skiprows = 1).T
+data_length = [len(data_xenolith), len(data_plate), len(data_adiabat), len(data_attenuation), len(data_viscosity)]
+n_data = int(np.sum(np.asarray(data_selection)*np.asarray(data_length)))
 
 param_selection = str(np.genfromtxt('./anelasticity_parameterisation/parameterisation_selection.txt', dtype='str'))
 priors = np.loadtxt('./anelasticity_parameterisation/'+param_selection+'/priors.txt', skiprows = 1)
 shutil.copyfile('./anelasticity_parameterisation/'+param_selection+'/thermodynamic_conversions.py', './likelihood/thermodynamic_conversions.py')
 
+# Initialise anelasticity model parameters randomly
+m0 = np.random.normal(loc = priors[0,:], scale = priors[1,:])
+# Initialise data hyperparameters randomly
+h0 = np.random.normal(loc = 0.0, scale = 1.0, size = n_data)
+# Initialise model
+x0 = np.concatenate((m0, h0))
+print(x0)
 
+n_trials = 1000
+n_burnin = int(0.5*n_trials)
+run_algorithm(n_trials, n_burnin, x0, priors, data_selection, data_length, data_xenolith, data_plate, data_adiabat, data_attenuation, data_viscosity)
