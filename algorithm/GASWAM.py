35af82418114b4d33c7a6cbc6ac40aa5330a6758
JamesHazzard <j.hazzard20@imperial.ac.uk>
2022-06-24 16:02:24 +0100

    preparing for inversion run

--- algorithm/GASWAM.py
+++ algorithm/GASWAM.py
@@ -37,7 +37,7 @@ def run_test_algorithm(n_trials, n_burnin, n_static, x0, m0, h0, priors, hyperpr
     t_init=time.time()
 
     for i in range(n_static):
-        if (i-1)%10 == 0:
+        if (i-1)%100 == 0:
             print(i) 
             #print(i, np.abs((n_accepted / i) - alpha_ideal), prior_x + likelihood_x)
             print(i, np.abs((n_accepted / i) - alpha_ideal), prior_x + likelihood_x, np.log10(RMS_x[0]) - x[n_m:])
@@ -66,7 +66,7 @@ def run_test_algorithm(n_trials, n_burnin, n_static, x0, m0, h0, priors, hyperpr
     C = np.cov(model[:,0:n_static]) # empirical covariance for first (n_static - 1) trials
 
     for i in range(n_static, n_trials): 
-        if i%10 == 0: 
+        if i%100 == 0: 
             #print(i, np.abs((n_accepted / i) - alpha_ideal), prior_x + likelihood_x)
             print(i, np.abs((n_accepted / i) - alpha_ideal), prior_x + likelihood_x, np.log10(RMS_x[0]) - x[n_m:])
             print(time.time() - t_init)
--- main.py
+++ main.py
@@ -8,13 +8,13 @@ import matplotlib.pyplot as plt
 
 t_start = time.time()
 
-now = create_output_directory(False) # get current time and produce an output directory using this timestamp
+now = create_output_directory(True) # get current time and produce an output directory using this timestamp
 print("Beginning inversion at", now)
 
 data, n_data = get_data() # retrieve inversion data
 x0, m0, h0, priors, hyperpriors = get_starting_model() # generate a starting model
 
-n_trials = 1000
+n_trials = 1600000
 n_burnin = int(0.5*n_trials)
 n_static = 99
 samples, RMS, track_posterior = run_test_algorithm(n_trials, n_burnin, n_static, x0, m0, h0, priors, hyperpriors, data, n_data)
@@ -29,4 +29,4 @@ RMS_labels = []
 for i in range(len(h_labels)):
     RMS_labels.append('RMS' + str(i + 1))
 x_labels = np.concatenate((['Posterior'], m_labels, h_labels, RMS_labels)) # Join labels
-#save_samples(stack, x_labels, now) # Save model samples
+save_samples(stack, x_labels, now) # Save model samples
