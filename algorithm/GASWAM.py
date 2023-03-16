dff8e21497b6d613c64b4fd35116e2bdae7400b4
JamesHazzard <j.hazzard20@imperial.ac.uk>
2023-03-16 16:33:44 +0000

    getting rid of unnecessary print calls

--- algorithm/GASWAM.py
+++ algorithm/GASWAM.py
@@ -25,7 +25,6 @@ def run_test_algorithm(n_trials, n_burnin, n_static, x0, m0, h0, priors, hyperpr
     prior_h = prior(hyperpriors[0,:], hyperpriors[1,:], h)
     prior_x = prior_m + prior_h
     likelihood_x, RMS_x = likelihood(data, m, h, n_xenolith, n_plate, n_adiabat, n_attenuation, n_viscosity)
-    print(prior_x, likelihood_x)
     proposal_priors = (priors[1,:] / 50)**2
     proposal_hyperpriors = np.full(n_h, 0.1**2)
     proposal_covariances = np.diag(np.concatenate((proposal_priors, proposal_hyperpriors)))
@@ -34,7 +33,6 @@ def run_test_algorithm(n_trials, n_burnin, n_static, x0, m0, h0, priors, hyperpr
     gamma = (2.38**2)/n_params
     model = np.zeros((n_params, n_trials))
     RMS = np.zeros((n_h_RMS, n_trials))
-    print(np.shape(RMS), n_h, np.shape(RMS_x), RMS_x)
     avg_model = np.zeros(n_params)
     track_posterior = np.zeros((1, n_trials))
     accepted_model = []
@@ -43,10 +41,7 @@ def run_test_algorithm(n_trials, n_burnin, n_static, x0, m0, h0, priors, hyperpr
 
     for i in range(n_static):
         if (i-1)%1000 == 0:
-            print(i) 
-            #print(i, np.abs((n_accepted / i) - alpha_ideal), prior_x + likelihood_x)
-            print(i, np.abs((n_accepted / i) - alpha_ideal), prior_x + likelihood_x, [np.log10(RMS_x[y] - x[n_m + y]) for y in range(len(x[n_m:]))])
-            print(time.time() - t_init)
+            print(i, time.time() - t_init, np.abs((n_accepted / i) - alpha_ideal), prior_x + likelihood_x, [np.log10(RMS_x[y] - x[n_m + y]) for y in range(len(x[n_m:]))])
             t_init = time.time()    
         model[:,i] = x
         RMS[:,i] = np.log10(RMS_x)
@@ -72,9 +67,7 @@ def run_test_algorithm(n_trials, n_burnin, n_static, x0, m0, h0, priors, hyperpr
 
     for i in range(n_static, n_trials): 
         if i%1000 == 0: 
-            #print(i, np.abs((n_accepted / i) - alpha_ideal), prior_x + likelihood_x)
-            print(i, np.abs((n_accepted / i) - alpha_ideal), prior_x + likelihood_x, [np.log10(RMS_x[y] - x[n_m + y]) for y in range(len(x[n_m:]))])
-            print(time.time() - t_init)
+            print(i, time.time() - t_init, np.abs((n_accepted / i) - alpha_ideal), prior_x + likelihood_x, [np.log10(RMS_x[y] - x[n_m + y]) for y in range(len(x[n_m:]))])
             t_init = time.time()
         track_posterior[0, i] = prior_x + likelihood_x
         model[:,i] = x
